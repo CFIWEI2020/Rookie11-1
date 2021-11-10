969849ec5a6f20cfbbf92161ddc327f5a7421a84
weiwi <marcoer@gmail.com>
2021-11-10 14:45:41 +0800

    Initial Commit

---
+++ Rookie11-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rookie11-1
+//
+//  Created by WEI-YI Wang on 2021/11/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
